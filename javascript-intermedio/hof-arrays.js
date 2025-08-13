427fac863f2ea26e358cc7d077acb331e14a9162
argeulimbo <argeusousaptbr@gmail.com>
2025-08-13 20:07:06 -0300

    final: Aula Javascript Intermediário I(funcional)

--- javascript-intermedio/hof-arrays.js
+++ javascript-intermedio/hof-arrays.js
@@ -2,6 +2,10 @@
     Funções de Alta Ordem (HOF) - Arrays
 */
 
+/*
+        1. Array.prototype.forEach()
+*/
+
 // Array de números
 const numeros = [40, 34, 67, 89, 23, 10];
 
@@ -22,3 +26,43 @@ console.log('---');
 
 // Usando forEach com uma função definida
 numeros.forEach(printElement);
+
+console.log("2. Array.prototype.find(): útil para encontrar um elemento em um Array")
+/*
+        2. Array.prototype.find(): útil para encontrar um elemento em um Array
+*/
+
+const encontrado = numeros.find((numero) => {
+    return numero > 10;
+})
+
+console.log("Usying find - " + encontrado);
+
+// Array de objetos
+const pessoas = [ 
+    {
+        nome: 'Marcia',
+        idade: 28,
+        sistema: 'GFP'
+    },
+    {
+        nome: 'Joao',
+        idade: 38,
+        sistema: 'GSP'
+    },
+    {
+        nome: 'Marcelo',
+        idade: 22,
+        sistema: 'TRB'
+    }
+];
+
+const pessoaEncontrada = pessoas.find((pessoa) => pessoa.nome == 'Marcia');
+console.log(pessoaEncontrada)
+
+/*
+        2. Array.prototype.findIndex(): útil para encontrar um elemento especificado por Index
+*/
+
+const pessoaEncontradaFindIndex = pessoas.find((pessoa) => pessoa.nome == 'Tutui');
+console.log(pessoaEncontradaFindIndex)
