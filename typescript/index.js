a6f761e54c809d70d72ddf2bc83347cbd8eca78d
argeulimbo <argeusousaptbr@gmail.com>
2025-08-20 16:50:48 -0300

    comments

--- typescript/index.js
+++ typescript/index.js
@@ -4,5 +4,30 @@
 */
 
 const aula = "JSON";
+const ferramenta = 'eslint';
+const fs = require('fs');
 
-console.log(aula);
+// 1. Lendo um arquivo JSON.
+
+fs.readFile('/typescript/eslint.js', (erro, dados) => {
+     if (erro) {
+          console.log('Erro ao ler o arquivo - ' + erro);
+     } else {
+          // dados: Buffer com os dados do arquivo json
+          console.log(JSON.pase(dados));
+          console.log(typeof dados);
+     }
+}); 
+
+// Convertendo JSON, em formato de string, para um objeto
+const jsonString = '{"nome": "Wallisson"}';
+console.log(JSON.parse(jsonString));
+
+// Convertendo um objeto do JS, em um JSON (String)
+
+const pessoa = {
+     nome: 'Tony Stark',
+     papel: 'Homem de Ferro',
+};
+
+console.log(JSON.stringify(pessoa));
