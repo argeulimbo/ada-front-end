1b387e9163c6ae46f83f33a630db279609e70a8d
argeulimbo <argeusousaptbr@gmail.com>
2025-09-18 11:32:46 -0300

    feat: update index.ts

--- typescript-poo/dist/index.js
+++ typescript-poo/dist/index.js
@@ -1,27 +1,25 @@
+"use strict";
 // Aula POO - Typescript
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Classe: abstração
 // Definindo abstração de uma pessoa
 class Pessoa {
-    nome: string;
-    idade: number;
-    altura: number;
-
-    constructor(nome: string, idade: number, altura: number) {
+    nome;
+    idade;
+    altura;
+    constructor(nome, idade, altura) {
         this.nome = nome;
         this.idade = idade;
         this.altura = altura;
     }
-
     dormir() {
         console.log('Dormindo...');
     }
-
     toString() {
         console.log('NOME: ' + this.nome + ' | Idade: ' + this.idade + ' | Altura: ' + this.altura);
     }
 }
-
 // Criando uma pessoa (objeto) a partir da classe Pessoa (abstração)
 const pessoa1 = new Pessoa('Argeu', 24, 1.80);
 console.log(pessoa1.toString());
+//# sourceMappingURL=index.js.map
