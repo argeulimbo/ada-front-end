06d4cfeec2cd825a3e985ab23654e8d586f48093
argeulimbo <argeusousaptbr@gmail.com>
2025-09-23 16:33:27 -0300

    feat: browser api

--- browser-api/js/index.js
+++ browser-api/js/index.js
@@ -9,5 +9,34 @@
 
 */
 
+// Pelo TagName - H1 e P
 const allH1 = document.getElementsByTagName("h1");
-console.log(allH1);
+console.log(allH1[0]);
+
+const allP = document.getElementsByTagName("p");
+console.log(allP);
+
+// Pelo Name - email
+const nameEmail = document.getElementsByName("email");
+console.log(nameEmail);
+
+// Pelo Id - logo
+const logoId = document.getElementById("js-logo");
+console.log(logoId);
+
+// QuerySelector
+const imagem = document.querySelector("body > img#js-logo");
+console.log(imagem);
+const primeiroParagrafo = document.querySelector("p.paragrafo");
+console.log(primeiroParagrafo);
+
+// QuerySelectorAll
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos);
+
+// Text Content e InnerHTML
+console.log("textContent: ", primeiroParagrafo.textContent);
+console.log("innerHTML: ", primeiroParagrafo.innerHTML);
+
+// Text Content = tudo é texto
+// InnerHTML = insere tags html dentro da string
