39ff53459248952532a5102f13dcac8e3dadaec1
argeulimbo <argeusousaptbr@gmail.com>
2025-09-24 11:24:51 -0300

    feat: update /browser-api-ii

--- browser-api-ii/js/index.js
+++ browser-api-ii/js/index.js
@@ -1,4 +1,22 @@
 /* Browser API - Escuta de Eventos e Estilos */
 
-const button = document.querySelector('section button');
-console.log(button.innerHTML)
+const aumentar = document.querySelector('#aumentar-botao');
+const diminuir = document.querySelector('#diminuir-botao');
+const input = document.querySelector('#input');
+
+const contagem = document.querySelector('#contador');
+console.log(contagem.innerHTML);
+
+aumentar.addEventListener('click', (event) => {
+    const valorAtual = Number(contagem.textContent);
+    contagem.textContent = valorAtual +1;
+});
+
+diminuir.addEventListener('click', (event) => {
+    const valorAtual = Number(contagem.textContent);
+    contagem.textContent = valorAtual -1;
+});
+
+input.addEventListener('input', (event) => {
+    console.log('" ' + input.value + ' "');
+})
